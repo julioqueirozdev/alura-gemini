73b9b14852370acb70dc83ed67e12861bf19d7aa
Júlio Queiroz <jcqaraujo@gmail.com>
2024-09-06 16:52:31 -0400

    finalização do projeto

--- app.js
+++ app.js
@@ -1,27 +1,48 @@
-let section = window.document.getElementById("resultados-pesquisa"); // Seleciona a seção onde os resultados serão exibidos
+function buscar() {
+  let section = window.document.getElementById("resultados-pesquisa"); // Seleciona a seção onde os resultados serão exibidos
 
-let resultados = ""; // String vazia para armazenar o HTML dos resultados
+  
 
-function buscar() { // Função para gerar e exibir os resultados da pesquisa
-  // Itera sobre cada dado da pesquisa e cria o HTML correspondente
+  let campoPesquisa = document.getElementById("busca").value;
+
+  if(!campoPesquisa){
+    section.innerHTML = '<p>Nenhum resultado foi encontrado! Busque um atleta.</p>'
+    return // finaliza o codigo caso entre neste condicional 
+  }
+
+  campoPesquisa =  campoPesquisa.toLowerCase()
+
+  // inicializa uma string vazia para armazenar os resultados 
+  let resultados = "";
+  let titulo = '';
+  let descricao = '';
+  let tags = '';
+
   for (let dado of dados) {
-    resultados += `
+
+    titulo = dado.titulo.toLowerCase()
+    descricao = dado.descricao.toLowerCase()
+    tags = dado.tags.toLowerCase()
+
+    if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa) ) {
+      resultados += `
       <div class="item-resultado"> 
-        <h2> 
-          <a href="https://www.www.instagram.com/rebecarandrade/" target="_blank" 
-             >${dado.titulo}</a> 
-        </h2>
+        <h2> <a href="https://www.www.instagram.com/rebecarandrade/" target="_blank">${dado.titulo}</a></h2>
         <p class="descricao-meta"> 
-          ${dado.descricao}
-        </p>
+          ${dado.descricao}</p>
         <a
           href='${dado.link}' 
           target="_blank"
           >Mais informções</a
         >
       </div>`;
+    }
+
   }
+  //se não existir resultados
+    if(!resultados) {
+      resultados = '<p>Nenhum resultado foi encontrado!</p>'
+    }
 
   section.innerHTML = resultados; // Insere o HTML dos resultados na seção
 }
-
--- dados.js
+++ dados.js
@@ -3,17 +3,20 @@ let dados = [
     {
         titulo: 'Rayssa Leal',
         descricao: 'Rayssa Leal, a Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Aos poucos anos, ela já coleciona títulos e inspira milhões de jovens.',
-        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal"
+        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags: 'skate fada sk8'
     },
     {
         titulo: 'Beatriz Souza',
         descricao: 'Beatriz Souza é uma judoca brasileira que se destaca pela sua técnica e determinação. Com diversas medalhas em competições nacionais e internacionais, ela é um dos grandes nomes do judô feminino brasileiro.',
-        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza" 
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+        tags: 'judô judo judoka' 
       },
       {
         titulo: 'Rebeca Andrade',
         descricao: 'Rebeca Andrade é uma ginasta brasileira que conquistou o coração dos brasileiros com suas performances impecáveis. Campeã olímpica, ela é um símbolo de superação e inspiração.',
-        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade" 
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags: 'ginástica ginastica ouro' 
       }
 ];
 
